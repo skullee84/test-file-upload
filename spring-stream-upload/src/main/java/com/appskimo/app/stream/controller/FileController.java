02a06c67f394886b258f7194121ee621e3a188c4
dominic <skullee84@gmail.com>
2022-06-14 02:39:24 +0900

    no message

--- spring-stream-upload/src/main/java/com/appskimo/app/stream/controller/FileController.java
+++ spring-stream-upload/src/main/java/com/appskimo/app/stream/controller/FileController.java
@@ -1,24 +1,52 @@
 package com.appskimo.app.stream.controller;
 
+import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.util.Streams;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import javax.servlet.http.Part;
+import java.io.*;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.UUID;
 
 @Slf4j
 @RestController
 public class FileController {
 
     private static final String ROOT = "/Users/dominic/temp/";
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+
+    @PostMapping(value = "/upload2", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> up(final HttpServletRequest request) throws IOException, ServletException {
+        Iterator<String> iterator = request.getHeaderNames().asIterator();
+        while (iterator.hasNext()) {
+            String headerName = iterator.next();
+            log.debug("## {} {}", headerName, request.getHeader(headerName));
+        }
+
+        @Cleanup ServletInputStream inputStream = request.getInputStream();
+        try (FileOutputStream outputStream = new FileOutputStream(ROOT + UUID.randomUUID(), false)) {
+            int read;
+            byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
+
+        return ResponseEntity.ok().build();
+    }
 
     @SneakyThrows
     @PostMapping("/upload")
